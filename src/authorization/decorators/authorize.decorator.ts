4c8c3205154c234fedece4c386b5144c539128db
privateOmega <privateOmega@protonmail.com>
2019-12-01 23:11:49 +0530

    added authorization method decorator

---
+++ src/authorization/decorators/authorize.decorator.ts
@@ -0,0 +1,13 @@
+import {MethodDecoratorFactory} from '@loopback/core';
+
+import {AuthorizationMetadata} from '../types';
+import {AUTHORIZATION_METADATA_ACCESSOR} from '../keys';
+
+export function authorize(permissions: string[]) {
+  return MethodDecoratorFactory.createDecorator<AuthorizationMetadata>(
+    AUTHORIZATION_METADATA_ACCESSOR,
+    {
+      permissions: permissions || [],
+    },
+  );
+}
