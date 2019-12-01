44a142ef6981e79f0d5a25bc65bcfe6145f33d48
privateOmega <privateOmega@protonmail.com>
2019-12-01 23:05:28 +0530

    added dependency injection for belongsto relation in userrole model repository

--- src/repositories/user-role.repository.ts
+++ src/repositories/user-role.repository.ts
@@ -1,15 +1,33 @@
-import {DefaultCrudRepository} from '@loopback/repository';
-import {inject} from '@loopback/core';
+import {
+  DefaultCrudRepository,
+  BelongsToAccessor,
+  repository,
+} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
 
-import {UserRole, UserRoleRelations} from '../models';
+import {UserRole, UserRoleRelations, User, Role} from '../models';
 import {PgDataSource} from '../datasources';
+import {UserRepository} from './user.repository';
+import {RoleRepository} from './role.repository';
 
 export class UserRoleRepository extends DefaultCrudRepository<
   UserRole,
   typeof UserRole.prototype.id,
   UserRoleRelations
 > {
-  constructor(@inject('datasources.pg') dataSource: PgDataSource) {
+  public readonly user: BelongsToAccessor<User, typeof User.prototype.id>;
+  public readonly role: BelongsToAccessor<Role, typeof Role.prototype.id>;
+
+  constructor(
+    @inject('datasources.pg') dataSource: PgDataSource,
+    @repository.getter('UserRepository')
+    userRepositoryGetter: Getter<UserRepository>,
+    @repository.getter('RoleRepository')
+    roleRepositoryGetter: Getter<RoleRepository>,
+  ) {
     super(UserRole, dataSource);
+
+    this.user = this.createBelongsToAccessorFor('user', userRepositoryGetter);
+    this.role = this.createBelongsToAccessorFor('role', roleRepositoryGetter);
   }
 }
